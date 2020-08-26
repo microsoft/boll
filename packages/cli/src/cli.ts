538220d8dc3856a26a7c3da3194e99f696e0ff23
JD Huntington <jdh@microsoft.com>
2020-08-26 14:59:08 -0700

    CLI integration

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,7 +1,15 @@
-import { ArgumentParser } from "argparse";
-import { Suite } from "./lib/suite";
-import { Logger } from "./lib/logger";
+import * as fs from "fs";
+import { bootstrapConfigurations } from "./config/bootstrap";
+import { Config } from "./lib/config";
 import { ConfigGenerator } from "./config-generator";
+import { ConfigRegistryInstance } from "./lib/config-registry";
+import { Logger } from "./lib/logger";
+import { RuleRegistryInstance } from "./lib/rule-registry";
+import { Suite } from "./lib/suite";
+import { ArgumentParser } from "argparse";
+import { promisify } from "util";
+import { resolve } from "path";
+const fileExistsAsync = promisify(fs.exists);
 
 const parser = new ArgumentParser({ description: "@boll/cli" });
 const subParser = parser.addSubparsers({
@@ -21,12 +29,15 @@ export enum Status {
 }
 
 export class Cli {
-  constructor(private logger: Logger, private suite: Suite) {}
+  private readonly configFileName = ".boll.config.js";
+
+  constructor(private logger: Logger) {}
 
   async run(args: string[]): Promise<Status> {
     const parsedCommand: ParsedCommand = parser.parseArgs(args);
     if (parsedCommand.command === "run") {
-      const result = await this.suite.run(this.logger);
+      const suite = await this.buildSuite();
+      const result = await suite.run(this.logger);
       result.errors.forEach((e) => {
         this.logger.error(e.text);
       });
@@ -40,5 +51,19 @@ export class Cli {
       return Status.Ok;
     }
     return Status.Error;
+  }
+
+  private async buildSuite(): Promise<Suite> {
+    const fullConfigPath = resolve(this.configFileName);
+    const exists = await fileExistsAsync(fullConfigPath);
+    if (!exists) {
+      this.logger.error(
+        `Unable to find ${fullConfigPath}; consider running "init" to create example config.`
+      );
+    }
+    bootstrapConfigurations();
+    const config = new Config(ConfigRegistryInstance, RuleRegistryInstance);
+    config.load(require(fullConfigPath));
+    return config.buildSuite();
   }
 }
