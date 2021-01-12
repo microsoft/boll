335acf20631b0d807cc056d8fac2c2f0f588f385
vipati <62967100+vipati@users.noreply.github.com>
2021-01-12 10:53:27 -0800

    fix boll line numbers for cross dependency rule (#68)
    * Fixing cross package dependency rule
    
    * Cross package dep test case
    
    Co-authored-by: Vivek Patil <vipati@microsoft.com>

--- packages/rules-typescript/src/tests/cross-package-dep-detector.test.ts
+++ packages/rules-typescript/src/tests/cross-package-dep-detector.test.ts
@@ -1,7 +1,8 @@
 import * as assert from "assert";
 import baretest from "baretest";
 import { CrossPackageDependencyDetector } from "../cross-package-dependency-detector";
-import { asBollDirectory, asBollFile, ResultStatus } from "@boll/core";
+import { asBollDirectory, asBollFile, getSourceFile, Failure, Package, ResultStatus } from "@boll/core";
+import { inFixtureDir } from "@boll/test-internal";
 export const test: any = baretest("Cross package dependency detector");
 
 test("Should pass if no cross-package dependencies detected", async () => {
@@ -25,4 +26,23 @@ test("Should fail if cross-package dependency detected", async () => {
   const result = sut.checkImportPaths(asBollDirectory("/a/b/c"), asBollFile("/a/b/c/baz.ts"), importPaths);
   assert.strictEqual(1, result.length);
   assert.strictEqual(ResultStatus.failure, result[0].status);
+});
+
+test("Should fail for missing imports if few imports are declared in devDependencies and devDeps mode is enabled", async () => {
+  await inFixtureDir("cross-package-dep-detector", __dirname, async () => {
+    const sut = new CrossPackageDependencyDetector();
+    const result = await sut.check(
+      await getSourceFile(asBollDirectory("."), "cross-package-dep-detector.ts", new Package({}, {}))
+    );
+    const failure = result[0] as Failure;
+    const failure1 = result[1] as Failure;
+    const failure2 = result[2] as Failure;
+    assert.strictEqual(3, result.length);
+    assert.strictEqual(ResultStatus.failure, failure.status);
+    assert.strictEqual(4, failure.line);
+    assert.strictEqual(ResultStatus.failure, failure1.status);
+    assert.strictEqual(8, failure1.line);
+    assert.strictEqual(ResultStatus.failure, failure2.status);
+    assert.strictEqual(9, failure2.line);
+  });
 });
