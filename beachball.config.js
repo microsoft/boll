676ce163322f2690bf4735c0ea43e0b1cd8dffea
Elizabeth Craig <elcraig@microsoft.com>
2025-02-20 13:31:34 -0800

    Update lock file deps and sort package.json

--- beachball.config.js
+++ beachball.config.js
@@ -1,10 +1,11 @@
 // @ts-check
 /** @type {import("beachball").BeachballConfig} */
 const config = {
+  access: "public",
   changehint: 'Run "yarn change" to generate a change file',
   branch: "main",
   groupChanges: true,
-  ignorePatterns: ["**/fixtures/**", "**/tests/**"],
+  ignorePatterns: ["**/fixtures/**/*", "**/tests/**/*"],
   disallowedChangeTypes: [
     "prerelease",
     // If a major release is needed, temporarily remove this line.
